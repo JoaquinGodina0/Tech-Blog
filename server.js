97a854cc1f32e7e0e94e01acaefece476c5191be
JoaquinGodina0 <godinajoaquin6@gmail.com>
2023-09-28 04:46:25 -0700

    fixed issue with server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const sess = {
 };
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
